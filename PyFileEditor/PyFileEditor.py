b47d36fd4b24bf1cef945a15fa19a0b0fe38c343
Valrravn <xtrashlove@gmail.com>
2023-02-09 15:39:51 +0000

    modify PY script

--- PyFileEditor/PyFileEditor.py
+++ PyFileEditor/PyFileEditor.py
@@ -1,15 +1,14 @@
 import this
 
 
-text = open('TextFile1.txt', 'r+')
+text = open('TextFile1.txt', 'r')
 lines = text.readlines()
 lines[0] = "Today date is " + lines[0]
 lines[1] = "Random number is " + lines[1]
 lines[2] = "Random Bloodborne boss is " + lines[2]
 text.close()
-open('TextFile1.txt', 'w').close()
 
-file = open('TextFile1.txt', 'r+')
+file  = open('TextFile1.txt', 'w')
 for line in lines:
     file.write(line)
 file.close()
